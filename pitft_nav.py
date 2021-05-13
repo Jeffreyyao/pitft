8971fad7d835941df890dc64d16ace797fd089fe
Jeffrey <jeffreyyao@outlook.com>
2021-05-12 18:31:57 -0600

    Update pitft_nav.py

--- pitft_nav.py
+++ pitft_nav.py
@@ -1,2 +1,30 @@
 from pitft_setup import *
 from PIL import Image, ImageDraw, ImageFont
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 20)
+
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+options = ["Time","Weather","Blog Gallery","Pi Status"]
+count = 4
+curr_idx = 0
+
+def choose(index):
+    clear()
+    for i in range(4):
+        if i==index:
+            draw.text((20,135//5*(i+1)), options[i], font=font, fill="#0000FF")
+        else:
+            draw.text((20,135//5*(i+1)), options[i], font=font, fill="#FFFFFF")
+    display.image(image)
+    sleep(1)
+
+choose(0)
+while 1:
+    if btnUp.is_pressed:
+        curr_idx = curr_idx-1 if curr_idx!=0 else count-1
+        choose(curr_idx)
+    elif btnDown.is_pressed:
+        curr_idx = curr_idx+1 if curr_idx!=count-1 else 0
+        choose(curr_idx)
