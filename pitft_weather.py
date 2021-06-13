5a6c6125dc04876b84eb69c580488778da980f9f
Jeffrey <jeffreyyao@outlook.com>
2021-06-12 21:37:03 -0600

    Update pitft_weather.py

--- pitft_weather.py
+++ pitft_weather.py
@@ -1,14 +1,10 @@
-from pitft import *
 from PIL import Image, ImageDraw, ImageFont
 import time
-from datetime import datetime
 import urllib.request as req
 import re
 import requests
-from signal import pause
-import os
 
-def weather_update():
+def weather_update(display,draw,font):
     req_url = "http://api.weatherapi.com/v1/current.json?key=c5fbb5f073b64c74ad651622211105&q=Boulder&aqi=no"
     res = req.urlopen(req_url).read()
 
@@ -16,12 +12,8 @@ def weather_update():
     temp = re.findall('temp_c":.+?,',str(res))[0].replace('temp_c":','').replace(',','')+" C"
     icon = "https://"+re.findall('icon":"//.+?png',str(res))[0].replace('icon":"//',"")
 
-    clear()
     draw.text((20,135//5), weather, font=font, fill="#FFFFFF")
     draw.text((20,135//5*2), temp, font=font, fill="#FFFFFF")
     draw.bitmap((100,135//5*2),Image.open(requests.get(icon, stream=True).raw))
     display.image(image)
     time.sleep(2)
-
-if __name__=="__main__":
-    weather_update()
