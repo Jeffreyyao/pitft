ec25fae736fe6df3db9b908e6e2a8009fe77e65a
Jeffrey <jeffreyyao@outlook.com>
2021-06-12 21:57:13 -0600

    Update pitft_time.py

--- pitft_time.py
+++ pitft_time.py
@@ -4,11 +4,6 @@ from datetime import datetime
 
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 135//2)
 
-def ret():
-    import pitft_nav
-
-btnDown.when_pressed = ret
-
 def time_update(display,image,draw):
     now = datetime.now()
     current_time = now.strftime("%H:%M")
