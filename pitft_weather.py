5835495c188d0a5d25fee61a9dc36b541da9f07b
Jeffrey <jeffreyyao@outlook.com>
2021-06-12 21:44:34 -0600

    Update pitft_weather.py

--- pitft_weather.py
+++ pitft_weather.py
@@ -6,14 +6,18 @@ import requests
 
 def weather_update(display,image,draw,font):
     req_url = "http://api.weatherapi.com/v1/current.json?key=c5fbb5f073b64c74ad651622211105&q=Boulder&aqi=no"
-    res = req.urlopen(req_url).read()
+    try:
+        res = req.urlopen(req_url).read()
+        weather = re.findall('text":".+?"',str(res))[0].replace('text":"','').replace('"','')
+        temp = re.findall('temp_c":.+?,',str(res))[0].replace('temp_c":','').replace(',','')+" C"
+        icon = "https://"+re.findall('icon":"//.+?png',str(res))[0].replace('icon":"//',"")
 
-    weather = re.findall('text":".+?"',str(res))[0].replace('text":"','').replace('"','')
-    temp = re.findall('temp_c":.+?,',str(res))[0].replace('temp_c":','').replace(',','')+" C"
-    icon = "https://"+re.findall('icon":"//.+?png',str(res))[0].replace('icon":"//',"")
-
-    draw.text((20,135//5), weather, font=font, fill="#FFFFFF")
-    draw.text((20,135//5*2), temp, font=font, fill="#FFFFFF")
-    draw.bitmap((100,135//5*2),Image.open(requests.get(icon, stream=True).raw))
-    display.image(image)
-    time.sleep(2)
+        draw.text((20,135//5), weather, font=font, fill="#FFFFFF")
+        draw.text((20,135//5*2), temp, font=font, fill="#FFFFFF")
+        draw.bitmap((100,135//5*2),Image.open(requests.get(icon, stream=True).raw))
+        display.image(image)
+        time.sleep(2)
+    except Exception:
+        draw.text((20,135//5), "Internet Excep.", font=font, fill="#FFFFFF")
+        display.image(image)
+        time.sleep(1)
